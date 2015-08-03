f6e577c544ae821909e94ccb77d197d0ed968566
beffe <beffe@beffe.de>
2015-08-04 00:22:10 +0200

    enabled static export

--- src/java/com/metamesh/opencms/rfs/RfsAwareDumpLoader.java
+++ src/java/com/metamesh/opencms/rfs/RfsAwareDumpLoader.java
@@ -178,7 +178,7 @@ public class RfsAwareDumpLoader extends CmsDumpLoader implements I_CmsResourceLo
    */
   public boolean isStaticExportEnabled() {
 
-      return false;
+      return true;
   }
 
   /**
